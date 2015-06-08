bacd9539102eef0dd65c27e920aea0588047662f
Wlodzimierz Borkowski <wlodek.borkowski@gmail.com>
2015-06-08 23:25:38 +0200

    add parser keywords

--- parsing.c
+++ parsing.c
@@ -39,8 +39,9 @@ int main(int argc, char* argv[]) {
     /* Define them with the following Language */
     mpca_lang(MPCA_LANG_DEFAULT,
               "                                                     \
-                number   : /-?[0-9]+/ ;                             \
-                operator : '+' | '-' | '*' | '/' ;                  \
+                number   : /[-+]?[0-9]+\.?[0-9]+/ ;                 \
+                operator : '+' | '-' | '*' | '/' | '%'              \
+                         | \"add\" | \"sub\" | \"mul\" | \"div\" ;  \                              \
                 expr     : <number> | '(' <operator> <expr>+ ')' ;  \
                 lispy    : /^/ <operator> <expr>+ /$/ ;             \
               ",
